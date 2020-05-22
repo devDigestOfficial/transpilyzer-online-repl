093864c776bd04bc1b4a95b1b6c16c5ba9ae599d
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2020-05-22 08:13:28 +0000

    Update Navbar.js
    fixed typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             <nav className="navbar navbar-dark bg-dark d-flex justify-content-center">
                 <a className="navbar-brand d-flex flex-row" href="/#">
                     <img src={Logo} width="48" height="auto" className="d-inline-block" alt=""/>
-                    <span className="h3 pl-2 mb-0">Tranpilyzer Online REPL</span>
+                    <span className="h3 pl-2 mb-0">Transpilyzer Online REPL</span>
                 </a>            
             </nav>
         </div>
